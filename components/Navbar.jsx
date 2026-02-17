30f5465e80cc2ce124815f3d6946e9306f1b12d3
Hasan Imam <labshasan@gmail.com>
2026-02-17 12:34:14 +0600

    completed the inital project structure , added db , added models

---
+++ components/Navbar.jsx
@@ -0,0 +1,45 @@
+
+import Link from "next/link"
+import Image from "next/image"
+
+const Navbar = ({ sidebar }) => {
+  return (
+    <nav>
+      <Link href="/">
+        <Image
+          src="/stayswift.svg"
+          alt="Stay Swift Logo"
+          width={200}
+          height={200}
+        />
+      </Link>
+      {sidebar && (
+        <ul>
+          <li>
+            <Link href="#">Recommended Places</Link>
+          </li>
+
+          <li>
+            <Link href="#">About Us</Link>
+          </li>
+
+          <li>
+            <Link href="#">Contact us</Link>
+          </li>
+
+          <li>
+            <Link href="/bookings">Bookings</Link>
+          </li>
+
+          <li>
+            <Link href="/login" class="login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar
